d32523f34dcdf3f6d4d6bd9b3f21dce9d716b1fe
farmerng46-spec <farmerng46@gmail.com>
2026-02-20 05:28:11 +0100

    Update KitEditorListener.java

--- Plugin/src/main/java/dev/lrxh/neptune/game/kit/listener/KitEditorListener.java
+++ Plugin/src/main/java/dev/lrxh/neptune/game/kit/listener/KitEditorListener.java
@@ -1,13 +1,17 @@
 package dev.lrxh.neptune.game.kit.listener;
 
 import dev.lrxh.neptune.API;
-import dev.lrxh.neptune.game.kit.command.KitEditorCommand;
+import dev.lrxh.neptune.configs.impl.MessagesLocale;
+import dev.lrxh.neptune.game.kit.Kit;
 import dev.lrxh.neptune.profile.data.ProfileState;
 import dev.lrxh.neptune.profile.impl.Profile;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+
+import java.util.Arrays;
 
 public class KitEditorListener implements Listener {
 
@@ -18,8 +22,18 @@ public class KitEditorListener implements Listener {
         if (profile == null)
             return;
         if (profile.hasState(ProfileState.IN_KIT_EDITOR)) {
-            // Richiama la logica che ora sta nel Command
-            KitEditorCommand.saveKitAndExit(player, profile);
+            Kit kit = profile.getGameData().getKitEditor();
+
+            profile.getGameData().get(kit)
+                    .setKitLoadout(Arrays.asList(player.getInventory().getContents()));
+
+            MessagesLocale.KIT_EDITOR_STOP.send(player.getUniqueId(), Placeholder.parsed("kit", kit.getDisplayName()));
+
+            if (profile.getGameData().getParty() == null) {
+                profile.setState(ProfileState.IN_LOBBY);
+            } else {
+                profile.setState(ProfileState.IN_PARTY);
+            }
         }
     }
 }
