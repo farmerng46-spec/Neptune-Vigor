0aaa8525b92591d3711e950f393974183d250b4b
farmerng46-spec <farmerng46@gmail.com>
2026-02-20 03:48:37 +0100

    Update QueueCheckTask.java

--- Plugin/src/main/java/dev/lrxh/neptune/feature/queue/tasks/QueueCheckTask.java
+++ Plugin/src/main/java/dev/lrxh/neptune/feature/queue/tasks/QueueCheckTask.java
@@ -95,22 +95,21 @@ public class QueueCheckTask extends NeptuneRunnable {
                 Participant participant2 = new Participant(player2);
 
                 MessagesLocale.MATCH_FOUND.send(uuid1, TagResolver.resolver(
-                        Placeholder.parsed("kit", kit.getDisplayName()),
-                        Placeholder.parsed("arena", arena.getDisplayName()),
-                        Placeholder.unparsed("opponent", participant2.getNameUnColored()),
-                        Placeholder.unparsed("opponent-ping", String.valueOf(ping2)),
-                        Placeholder.unparsed("opponent-elo", String.valueOf(profile2.getGameData().get(kit).getElo())),
-                        Placeholder.unparsed("elo", String.valueOf(profile1.getGameData().get(kit).getElo())),
-                        Placeholder.unparsed("ping", String.valueOf(ping1))));
+                Placeholder.parsed("kit", kit.getDisplayName()),
+                Placeholder.parsed("arena", arena.getDisplayName()),
+                Placeholder.unparsed("opponent", participant2.getNameUnColored()),
+                Placeholder.unparsed("opponent-ping", String.valueOf(ping2)),
+                Placeholder.unparsed("ping", String.valueOf(ping1))
+                SoundLocale.MATCH_FOUND.play(uuid1);
+
 
                 MessagesLocale.MATCH_FOUND.send(uuid2, TagResolver.resolver(
                         Placeholder.parsed("kit", kit.getDisplayName()),
                         Placeholder.parsed("arena", arena.getDisplayName()),
                         Placeholder.unparsed("opponent", participant1.getNameUnColored()),
                         Placeholder.unparsed("opponent-ping", String.valueOf(ping1)),
-                        Placeholder.unparsed("opponent-elo", String.valueOf(profile1.getGameData().get(kit).getElo())),
-                        Placeholder.unparsed("elo", String.valueOf(profile2.getGameData().get(kit).getElo())),
                         Placeholder.unparsed("ping", String.valueOf(ping2))));
+                        SoundLocale.MATCH_FOUND.play(uuid2);
 
                 TaskScheduler.get().startTaskCurrentTick(new NeptuneRunnable() {
                     @Override
